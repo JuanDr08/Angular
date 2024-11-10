7ab80b37b00fe1ff32a892d44fbf35a9a3e65d3e
JuanDr08 <156454177+JuanDr08@users.noreply.github.com>
2024-11-10 16:54:14 -0500

    feat: :sparkles: From ts to HTML, flow control new sintax

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,16 +1,21 @@
 import { Component } from '@angular/core';
 import { routes } from '../../app.routes';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
 	selector: 'app-sidemenu',
 	standalone: true,
-	imports: [],
+	imports: [
+		CommonModule, 
+		RouterModule, // Habilitamos el uso del modulo de rutas para usar enrutamientos
+	],
 	templateUrl: './sidemenu.component.html',
 	styleUrl: './sidemenu.component.css'
 })
 export class SidemenuComponent {
 
-	public menuItems= routes
+	public menuItems = routes
 	.map(route => route.children ?? [])
 	.flat()
 	.filter(route => route && route.path && !route.path?.includes(':'));
